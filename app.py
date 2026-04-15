0669043bca0a5e868f3be553f41e35be8a17b547
Varshakp44 <1nc25mc025@ncetmail.com>
2026-04-15 11:09:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from gorq import Groq
+st.set_page_config("PragyanAI Content generator",layout="Wide')
+st.title("PragyanAi_content Generator")
+st.image(f1.jpg)              
